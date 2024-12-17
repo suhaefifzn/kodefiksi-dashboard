b624d92cc7754bd40ce02947753334bd46daf477
suhaefifzn <suhaefi21@gmail.com>
2024-12-17 12:12:15 +0700

    update: change favicon path

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Kode Fiksi {{ isset($title) ? ' - ' . $title : '' }}</title>
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="shortcut icon" href="/assets/images/logo.png" />
+    {{-- Favicons --}}
+    <link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/assets/favicon/site.webmanifest" />
   </head>
   <body>
     <div class="container-scroller">
